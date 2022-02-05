49c827d8af7fd4380e2d3f9b4d0ef646f309f5b0
Illia Hulenko <ihulenk1@educacion.navarra.es>
2022-02-05 17:31:42 +0100

    Simplest shopping list is done

---
+++ src/main/java/com/example/shoping_list_Spring/persist/ShoppingItemRepository.java
@@ -0,0 +1,8 @@
+package com.example.shoping_list_Spring.persist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShoppingItemRepository extends JpaRepository<ShoppingItem, Long> {
+}
